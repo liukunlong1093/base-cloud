cf0fbe03de683fae594088d58ff08ec3c5147374
liukunlong1093 <liukunlong1093@163.com>
2018-03-16 15:21:50 +0800

    增加注释及代码调整

--- template-manager/template-gateway/src/main/java/com/mes/filter/AccessFilter.java
+++ template-manager/template-gateway/src/main/java/com/mes/filter/AccessFilter.java
@@ -1,13 +1,5 @@
 package com.mes.filter;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.alibaba.fastjson.JSON;
 import com.mes.core.exception.SystemException;
 import com.mes.core.exception.enums.SystemExceptionEnums;
@@ -17,6 +9,12 @@ import com.mes.core.utils.InterfaceAuthenUtils;
 import com.mes.core.utils.StringUtils;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccessFilter extends ZuulFilter {
 	private static final Logger logger = LoggerFactory.getLogger(AccessFilter.class);
@@ -28,6 +26,7 @@ public class AccessFilter extends ZuulFilter {
 	 * post：在routing和error过滤器之后被调用
 	 * error：处理请求时发生错误时被调用
 	 */
+	@Override
 	public String filterType() {
 		return "pre";
 	}
@@ -35,6 +34,7 @@ public class AccessFilter extends ZuulFilter {
 	/**
 	 * 通过int值来定义过滤器的执行顺序数值越小优先级越高。
 	 */
+	@Override
 	public int filterOrder() {
 		return 0;
 	}
@@ -43,6 +43,7 @@ public class AccessFilter extends ZuulFilter {
 	 * 通过此方法来指定过滤器的有效范围
 	 * 返回一个boolean类型来判断该过滤器是否要执行，所以通过此函数可实现过滤器的开关
 	 */
+	@Override
 	public boolean shouldFilter() {
 		return true;
 	}
@@ -52,20 +53,21 @@ public class AccessFilter extends ZuulFilter {
 	 * 不对其进行路由，然后通过ctx.setResponseStatusCode(401)设置了其返回的错误码，当然我们也可
 	 * 以进一步优化我们的返回，比如，通过ctx.setResponseBody(body)对返回body内容进行编辑等。
 	 */
+	@Override
 	public Object run() {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
 		logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-		/** token*/
+		//token
 		String token = request.getHeader("token");
 
-		/** 签名*/
+		//签名
 		String sign = request.getHeader("sign");
 
-		/** 时间*/
+		//时间
 		String timestamp = request.getHeader("timestamp");
 
-		/** 请求地址*/
+		//请求地址
 		// String url = request.getRequestURI();
 
 		String visitUrl = request.getRequestURL().toString();
@@ -74,9 +76,12 @@ public class AccessFilter extends ZuulFilter {
 		String projectName = request.getContextPath();
 		String weburl = sitePart + ":" + port + projectName + "/";
 		String actonUrl = visitUrl.replace(weburl, "");
-		String action = actonUrl;// 当前调用的方法
-		Map<String, String> noFilterActionMap = new HashMap<String, String>();// todo
-		if (noFilterActionMap.containsKey(action)) {// 如果当前是非过滤的的Action方法名则不作授权认证
+		// 当前调用的方法
+		String action = actonUrl;
+		// 待实现逻辑
+		Map<String, String> noFilterActionMap = new HashMap<String, String>();
+		// 如果当前是非过滤的的Action方法名则不作授权认证
+		if (noFilterActionMap.containsKey(action)) {
 			return null;
 		} else {
 			try {
@@ -84,8 +89,8 @@ public class AccessFilter extends ZuulFilter {
 				if (!StringUtils.hasText(token) || !StringUtils.hasText(sign) || !StringUtils.hasText(timestamp)) {
 					throw new SystemException(SystemExceptionEnums.MISSING_REQUIRED_ARGUMENTS);
 				}
-				// token在缓存中的信息
-				Map<Object, Object> tokenMap = new HashMap<Object, Object>();// todo;
+				// token在缓存中的信息 todo;
+				Map<Object, Object> tokenMap = new HashMap<>(100);
 				tokenMap.put("secret", "VehiR3orHJa7agNmfHdI3UxKvFHMcQAsS+GYz3mQsicTpmyABiqD9mSB0GQyfiZat/2opfVtcjK7O7mRrUtW7g==");
 				if (tokenMap == null || tokenMap.size() <= 0) {
 					throw new SystemException(SystemExceptionEnums.TOKEN_EXPIRED);
